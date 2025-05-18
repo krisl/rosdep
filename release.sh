b5711dbab56e496f505e66aeb723334070b8d11b
Aaron Lipinski <kris.lipinski@gmail.com>
2025-05-18 13:38:48 +0200

    release script

--- release.sh
+++ release.sh
@@ -1,6 +1,19 @@
-python3 -m build --sdist .
-python3 -m build --wheel .
+#!/bin/bash
+
+#python3 -m build --sdist .
+#python3 -m build --wheel .
 #ls dist
 #twine upload dist/rosdep2-0.25.1.tar.gz dist/rosdep2-0.25.1-py3-none-any.whl
 #vim ~/.pypirc
 #twine upload dist/rosdep2-0.25.1.tar.gz dist/rosdep2-0.25.1-py3-none-any.whl
+
+LAST_LINE="$(python3 -m build --sdist --wheel | tail -1)"
+
+echo $LAST_LINE
+
+
+echo "-------------"
+
+BITS="$(echo $LAST_LINE | cut -d ' ' -f 3,5)"
+
+echo "twine dist/$(echo $BITS | cut -d ' ' -f 1) dist/$(echo $BITS | cut -d ' ' -f 2)"
